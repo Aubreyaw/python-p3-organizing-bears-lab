f59b771532e627db7ec9eb5317b8ad1b8d57e555
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-22 00:14:19 -0400

    Completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Grumpy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 5, "F", "Black", "A handful", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 2, "F", "White", "Quiet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 7, "M", "Black", "disciplined", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 4, "F", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 5, "F", "Brown", "Sweet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 8, "F", "Black", "Bossy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, "M", "White", "Timid", 1);
